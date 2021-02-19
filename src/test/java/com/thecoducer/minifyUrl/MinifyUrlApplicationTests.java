e9449f1ec19cf51fe5e887e8ba6bc129906210a8
Mayukh Datta <mayukh5741@gmail.com>
2021-02-19 14:13:00 +0530

    initial setup

---
+++ src/test/java/com/thecoducer/minifyUrl/MinifyUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecoducer.minifyUrl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinifyUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
